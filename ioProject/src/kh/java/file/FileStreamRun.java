5a76dafc91af4621a6ca4a7ad9a91509992941ae
user2 <user2@user2-PC>
2018-03-13 16:08:24 +0900

    test

---
+++ ioProject/src/kh/java/file/FileStreamRun.java
@@ -0,0 +1,14 @@
+package kh.java.file;
+
+public class FileStreamRun {
+
+	public static void main(String[] args) {
+		FileStreamTest fst=new FileStreamTest();
+//		fst.saveFile();
+//		fst.readFile();
+//		fst.saveFileChar();
+//		fst.readFileChar();
+		fst.copyFile();
+	}
+
+}
